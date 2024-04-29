6ada1e9c1628ad15aa40a288f4d33e5631c02dec
TaghreedSamir94 <taghreedsamir49@gmail.com>
2024-04-29 11:03:54 +0300

    2nd session

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Mycontroller;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,50 @@ Route::get('/', function () {
 // Route::get('taghreed/{id}',function($id){
 //     return 'Welcome to My First-website ' .$id;
 // });
+
+// Route::get('taghreed/{id?}',function($id=209){
+//     return "welcome".$id;
+// })->where(['id'=>'[0-9]+']);
+
+// Route::get('taghreed/{id?}',function($id=209){
+//     return "welcome".$id;
+// })->whereNumber('id');
+
+Route::get('course/{name}',function($name){
+    return "welcome to ".$name ." course";
+})->whereAlpha('name');
+//////////////////////////////////////////////////
+Route::get('course/{name}',function($name){
+    return "welcome to ".$name ." course";
+})->whereIn('name',['php','java']);
+
+// /////////////////////////////////////////////
+Route::prefix('cars')->group(function(){
+    Route::get('bmw',function(){
+        return 'category is bmw';
+    });
+    Route::get('marcedees',function(){
+        return 'category is marcedees';
+    });
+
+});
+//////////////////////////////////////////////////////
+
+// Route::fallback(function(){
+//     return redirect('/');
+// });
+
+//////////////////////////////////////////////////
+// Route::get('taghreedFiles',function(){
+//      return view('test');
+// });
+
+Route::get('taghreedForms',function(){
+    return view('form1');
+});
+
+Route::post('recData',function(){
+    return 'data received';
+})->name('recForm1');
+/////////////////////////////////////
+Route::get('test209',[Mycontroller::class,'my_data']);
