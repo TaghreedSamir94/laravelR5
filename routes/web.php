1e27ae4f255abac9f24529a8be09ecee701f6b82
TaghreedSamir94 <taghreedsamir49@gmail.com>
2024-04-29 07:09:30 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('taghreed',function(){
+//     return 'Welcome to My First-website';
+// });
+
+// Route::get('taghreed/{id}',function($id){
+//     return 'Welcome to My First-website ' .$id;
+// });
